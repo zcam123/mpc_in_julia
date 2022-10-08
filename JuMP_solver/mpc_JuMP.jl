2136cf294fbdce0a9fe9bbf9f96564163a79f785
zcam123 <zcamenard@gmail.com>
2022-10-08 19:44:11 -0400

    Minor typo fixes

--- JuMP_solver/mpc_JuMP.jl
+++ JuMP_solver/mpc_JuMP.jl
@@ -6,7 +6,7 @@ using Plots
 neuron = Model(Ipopt.Optimizer)
 set_silent(neuron)
 
-#defining all parameters of for model and probem
+#defining all parameters for model and probem
 A = [1 -6.66e-13 -2.03e-9 -4.14e-6;
      9.83e-4 1 -4.09e-8 -8.32e-5;
      4.83e-7 9.83e-4 1 -5.34e-4;
@@ -17,11 +17,11 @@ B = [9.83e-4, 4.83e-7, 1.58e-10, 3.89e-14]
 
 C = [-.0096 .0135 .005 -.0095]
 
-#functions based on Ax + Bu to give each part of the x vector
-x1next(x1, x2, x3, x4, u) = x1 - 6.66*10^(-13)*x2 - 2.03*10^(-9)*x3 - 4.14*10^(-6)*x4 + 94.3*u
-x2next(x1, x2, x3, x4, u) = 0.000983*x1 + x2 - 4.09*10^(-8)*x3 - 0.0000832*x4 + 41.3*u
-x3next(x1, x2, x3, x4, u) = 4.83*10^(-7)*x1 + 0.000983*x2 + x3 - 0.000534*x4 + u*1.58*10^-10
-x4next(x1, x2, x3, x4, u) = 1.58*10^(-10)*x1 + 4.83*10^(-7)*x2 + 0.000983*x3 + 0.9994*x4 + u*3.89*10^-14
+#functions based on Ax + Bu to give each part of the x vector - used as storing components of x vector seperately is more convenient with their setup
+# x1next(x1, x2, x3, x4, u) = x1 - 6.66*10^(-13)*x2 - 2.03*10^(-9)*x3 - 4.14*10^(-6)*x4 + 94.3*u
+# x2next(x1, x2, x3, x4, u) = 0.000983*x1 + x2 - 4.09*10^(-8)*x3 - 0.0000832*x4 + 41.3*u
+# x3next(x1, x2, x3, x4, u) = 4.83*10^(-7)*x1 + 0.000983*x2 + x3 - 0.000534*x4 + u*1.58*10^-10
+# x4next(x1, x2, x3, x4, u) = 1.58*10^(-10)*x1 + 4.83*10^(-7)*x2 + 0.000983*x3 + 0.9994*x4 + u*3.89*10^-14
 
 #function based on C*x to get y  
 x_to_y(x) = -0.0096*x[1] + 0.0135*x[2] + 0.005*x[3] -0.0095*x[4]
@@ -63,7 +63,7 @@ J0 = (yD-y0)^2
 fix(J[1], J0; force = true)
 
 
-#Expressions for use in determining state values
+#Expressions for use in determining error values
 @NLexpressions(
     neuron,
     begin
@@ -73,7 +73,7 @@ fix(J[1], J0; force = true)
 );
 
 for j in 2:pred
-    #x vector dynamics
+    #x vector dynamics - from Ax + Bu
     @NLconstraint(neuron, x1[j] == x1[j-1] - 6.66*10^(-13)*x2[j-1] - 2.03*10^(-9)*x3[j-1] - 4.14*10^(-6)*x4[j-1] + 94.3*u[j-1])
     @NLconstraint(neuron, x2[j] == 0.000983*x1[j-1] + x2[j-1] - 4.09*10^(-8)*x3[j-1] - 0.0000832*x4[j-1] + 41.3*u[j-1])
     @NLconstraint(neuron, x3[j] == 4.83*10^(-7)*x1[j-1] + 0.000983*x2[j-1] + x3[j-1] - 0.000534*x4[j-1] + u[j-1]*1.58*10^-10)
